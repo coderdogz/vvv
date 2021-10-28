10e57f39cf8ad4ece043aa325520df7223b504e0
coderdogz <17637462979@sina.cn>
2021-10-28 21:06:46 +0800

    坤坤上传

--- app/src/main/java/com/zral/ikun/MainActivity.java
+++ app/src/main/java/com/zral/ikun/MainActivity.java
@@ -2,7 +2,18 @@ package com.zral.ikun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.os.Vibrator;
+import android.app.Service;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView image1 = (ImageView) findViewById(R.id.img1);
+        Bitmap bitmap = getLoacalBitmap("ikun1.png");
+        image1.setImageBitmap(bitmap);
+        Vibrator vibrator = (Vibrator) getSystemService(Service.VIBRATOR_SERVICE);
+        vibrator.vibrate(1000000);
+    }
+
+    private Bitmap getLoacalBitmap(String url) {
+        AssetManager assetManager = getAssets();
+        try (InputStream is = assetManager.open(url)) {
+            return BitmapFactory.decodeStream(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+        return null;
     }
 }
